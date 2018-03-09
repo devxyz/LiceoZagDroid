f4521b9e57252f75c1c7f48d51e8da88ac7f3c57
devxyz <stefano.millozzi@gmail.com>
2018-03-09 18:17:51 +0100

    v1.0

--- LiceoZagDroidApp/src/it/gov/scuolesuperioridizagarolo/db/ManagerArticolo.java
+++ LiceoZagDroidApp/src/it/gov/scuolesuperioridizagarolo/db/ManagerArticolo.java
@@ -1,11 +1,14 @@
 package it.gov.scuolesuperioridizagarolo.db;
 
 import it.gov.scuolesuperioridizagarolo.dao.*;
+import it.gov.scuolesuperioridizagarolo.model.ArticoloSdoContainer;
 import org.greenrobot.greendao.query.DeleteQuery;
 import org.greenrobot.greendao.query.QueryBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by stefano on 02/02/2018.
@@ -27,7 +30,6 @@ public class ManagerArticolo {
         if (unique == null) return -1;
         return unique.getRemoteId();
     }
-
 
     //rimuove tutti gli articoli con remoteid minore di quello specificato
     public void removeAllLess(int minRemoteId) {
@@ -58,7 +60,37 @@ public class ManagerArticolo {
         //=========================================================
         final DeleteQuery<ArticoloDB> queryDeleteArticleDB = elencoArticoliDaRimuovere.buildDelete();
         queryDeleteArticleDB.executeDeleteWithoutDetachingEntities();
-
-
     }
+
+    public ArticoloSdoContainer allArticle() {
+        final List<AttachmentArticoloDB> listAttachment = session.getAttachmentArticoloDBDao().queryBuilder().list();
+        final List<TagArticoloDB> listTag = session.getTagArticoloDBDao().queryBuilder().list();
+        final List<ArticoloDB> listArticolo = session.getArticoloDBDao().queryBuilder().list();
+
+        final Map<Long, ArticoloSdoContainer.ArticoloSdo> map = new TreeMap<>();
+
+        //articolo
+        for (ArticoloDB a : listArticolo) {
+            ArticoloSdoContainer.ArticoloSdo s = new ArticoloSdoContainer.ArticoloSdo(a);
+            map.put(a.getId(), s);
+        }
+
+        //tag
+        for (TagArticoloDB t : listTag) {
+            map.get(t.getFk_articleId()).tags.add(t);
+        }
+
+        //attachment
+        for (AttachmentArticoloDB t : listAttachment) {
+            map.get(t.getFk_articleId()).attachments.add(t);
+        }
+
+        //articoli
+        ArticoloSdoContainer s = new ArticoloSdoContainer();
+        s.articoli.addAll(map.values());
+
+        return s;
+    }
+
+
 }
